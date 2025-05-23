ec665a3cd0b33930d2028c1b7f843936df9c1ba7
phoo <PHOOPYAESA28.24@ichat.sp.edu.sg>
2025-05-23 19:32:23 +0800

    exercise2 done

--- Lab3.py
+++ Lab3.py
@@ -5,14 +5,19 @@ SORT_DESCENDING = 1
 
 
 def bubble_sort(arr, sorting_order):
+    for index in arr: 
+        if type(index) is not int: 
+            return 2
 
     # Copy input list to results list
     arr_result = arr.copy()
 
     # Get number of elements in the list
     n = len(arr_result)
+    if n == 0 : 
+        return 0
 
-    if n < 10:
+    elif n < 10:
         # Traverse through all array elements
         for i in range(n - 1):
             # range(n) also work but outer loop will
@@ -33,6 +38,9 @@ def bubble_sort(arr, sorting_order):
                 else:
                     # Return an empty array
                     arr_result = []
+
+    elif n >10 : 
+        arr_result = 1
     else:
         arr_result = -1
 
